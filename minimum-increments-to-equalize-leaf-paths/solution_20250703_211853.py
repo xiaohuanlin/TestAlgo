8da38b7819cc65d05a2bdd7e7fc00d171dd3ebd3
xiaohuanlin <33860497+xiaohuanlin@users.noreply.github.com>
2025-07-13 15:25:12 -0400

    Minimum Increments to Equalize Leaf Paths - 20250703

---
+++ minimum-increments-to-equalize-leaf-paths/solution_20250703_211853.py
@@ -0,0 +1,33 @@
+# File: solution
+# Problem: Minimum Increments to Equalize Leaf Paths
+# Status: Accepted
+# Language: python3
+# Submit Time: 2025-07-03 21:18:53
+# Runtime: 269 ms
+# Memory: 72.3 MB
+
+class Solution:
+    def minIncrease(self, n: int, edges: List[List[int]], cost: List[int]) -> int:
+        m = defaultdict(list)
+        for u, v in edges:
+            m[u].append(v)
+            m[v].append(u)
+
+        def dfs(node, parent):
+            maxv = 0
+            maxc = 0
+            cnt = 0
+            for v in m[node]:
+                if v == parent:
+                    continue
+                cnt += 1
+                changed, total = dfs(v, node)
+                cnt += changed
+                if maxv < total:
+                    maxv = total
+                    maxc = 1
+                elif maxv == total:
+                    maxc += 1
+            return cnt - maxc, maxv + cost[node]
+
+        return dfs(0, None)[0]
